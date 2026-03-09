3ab7894ade3f7264849142f5ed30416cd6df87b9
Malik Blackman <malikblackman99@gmail.com>
2026-03-09 10:12:56 -0400

    done

---
+++ public/script.js
@@ -0,0 +1,17 @@
+console.log(window.navigator.cookieEnabled);
+console.log(window.navigator.onLine);
+console.log(navigator.appVersion);
+console.log(navigator.userAgent)
+console.log(navigator.platform);
+
+console.log(window.location.href);
+console.log(window.location.protocol);
+console.log(window.location.hostname);
+
+function redirect(url){
+  window.location.assign(url);
+}
+
+window.onload = function(event){
+   console.log("Page has loaded");
+}
